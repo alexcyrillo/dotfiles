40c2b76496d2d71a926ba617826a9b238666949d
Alex Cyrillo <alex.csborges@hotmail.com>
2025-10-29 16:59:39 -0300

    Atualizar dotfiles

--- zsh/custom/paths.zsh
+++ zsh/custom/paths.zsh
@@ -1,1 +1,19 @@
+export DEV_HOME="$HOME/Dev"
+
+
 export PATH="$PATH:$HOME/.local/bin"
+
+export PATH="$DEV_HOME/flutter/bin:$PATH"
+export PATH="$DEV_HOME/.fvm_flutter/bin:$PATH"
+
+export ANDROID_HOME="$DEV_HOME/Android/Sdk"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/emulator:$PATH"
+export PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+export ANDROID_AVD_HOME="$DEV_HOME/.config/.android/avd"
+
+
+export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+
